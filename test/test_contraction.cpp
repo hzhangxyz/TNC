71b969109ea18a666d32e9270405a3af34732b07
Hao Zhang <hao.zhang.xyz@gmail.com>
2019-01-26 01:15:10 +0800

    好像确实有问题

--- test/test_contraction.cpp
+++ test/test_contraction.cpp
@@ -1,13 +1,13 @@
-#include "eigen_node.hpp"
+#include <Eigen/CXX11/Tensor>
 #include <iostream>
 
 int main(){
-    Eigen::Tensor<int, 2> a(2, 3);
-    a.setValues({{1, 2, 3}, {6, 5, 4}});
+    Eigen::Tensor<int, 1> a(3);
+    a.setRandom();
     Eigen::Tensor<int, 2> b(3, 2);
     b.setValues({{1, 2}, {4, 5}, {5, 6}});
 
-    // Compute the traditional matrix product
-    Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
-    Eigen::Tensor<int, 2> AB = a.contract(b, product_dims);
+    Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(0, 0) };
+    auto AB = a.contract(b, product_dims);
+    std::cout << AB.NumDimensions << "\n" ;
 }
