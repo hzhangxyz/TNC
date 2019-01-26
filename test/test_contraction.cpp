5f91f74d9f1b6659b04ec672a78ae48672ea2868
Hao Zhang <zh970205@mail.ustc.edu.cn>
2019-01-27 01:47:36 +0800

    给eigenpatch中加个ifdef

--- test/test_contraction.cpp
+++ test/test_contraction.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 int main(){
-    Eigen::Tensor<int, 1> a(3);
-    a.setRandom();
-    Eigen::Tensor<int, 2> b(3, 2);
-    b.setValues({{1, 2}, {4, 5}, {5, 6}});
+    Eigen::Tensor<double, 4> T (2, 2, 2, 2);
+    T.setRandom();
+    Eigen::Tensor<double, 2> A (2, 2);
+    A.setRandom();
 
     Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(0, 0) };
-    auto AB = a.contract(b, product_dims);
-    std::cout << AB.NumDimensions << "\n" ;
+    Eigen::Tensor<double, 4> M = T.contract(A, product_dims);
+    std::cout << M.NumDimensions << "\n" ;
 }
