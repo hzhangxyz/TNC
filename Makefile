49c13af55c31ea2e757526774f6e6adbf3ffbfe5
Hao Zhang <zh970205@outlook.com>
2019-01-28 09:43:57 +0800

    好像可以用了

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CXX=clang++
 CXXFLAGS+= -I./eigen-git-mirror -I./eigen-git-mirror/unsupported -lblas -llapacke --std=c++11
 
-ifeq ($(DEBUG), YES)
-	CXXFLAGS+= -g
+DEBUG?=0
+ifeq ($(DEBUG), 0)
+	CXXFLAGS+= -O3 -march=native
 else
-	CXXFLAGS+= -O3 -march=native 
+	CXXFLAGS+= -g
 endif
 
 object = main mps_heisenburg
--- mps_heisenburg.cpp
+++ mps_heisenburg.cpp
@@ -1,26 +1,129 @@
 #include "eigen_node.hpp"
 
-template<int D, int L, typename Base=double>
+template<typename Base=double>
 struct MPS
 {
+    int D;
+    int L;
     static const int Dphy = 2;
+    Base delta_t = Base(0.01);
+    Eigen::Tensor<Base, 4> Update;
+    Base Identity_data[16] = {
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1};
     Base Hamiltonian_data[16] = {
         1, 0, 0, 0,
         0,-1, 2, 0,
         0, 2,-1, 0,
         0, 0, 0, 1};
-    Eigen::TensorMap<Eigen::Tensor<Base, 4>> Hamiltonian = {
-        (Base *)Hamiltonian_data,
-        {2, 2, 2, 2}
-    };
 
-    std::array<Eigen::Tensor<Base, 3>, L> lattice;
+    Eigen::Tensor<Base, 4> Hamiltonian;
+    Eigen::Tensor<Base, 4> Identity;
+
+    std::vector<Eigen::Tensor<Base, 3>> lattice;
+    std::vector<Eigen::Tensor<Base, 4>> __left_contract;
+    std::vector<Eigen::Tensor<Base, 4>> __right_contract; 
     //std::array<Eigen::Tensor<Base, 1>, L-1> env;
 
-    MPS()
+    Eigen::Tensor<Base, 4> left_contract(int i)
     {
+        if(i==-1)
+        {
+            Eigen::Tensor<Base, 4> res(1,1,1,1);
+            res(0,0,0,0) = 1;
+            res.leg_info = {Left1, Left2, Right1, Right2};
+            return res;
+        }
+        if(__left_contract[i].size()!=0)
+        {
+            return __left_contract[i];
+        }
+        if(i==0)
+        {
+            __left_contract[i] = Node::contract<1>(
+                lattice[i], lattice[i],
+                {Phy}, {Phy},
+                {{Left, Left1}, {Right, Right1}},
+                {{Left, Left2}, {Right, Right2}}
+            );
+        }
+        else
+        {
+            __left_contract[i] = Node::contract<2>(
+                left_contract(i-1),
+                Node::contract<1>(
+                    lattice[i],
+                    lattice[i],
+                    {Phy}, {Phy},
+                    {{Left, Left1}, {Right, Right1}},
+                    {{Left, Left2}, {Right, Right2}}
+                ),
+                {Right1, Right2}, {Left1, Left2});
+        }
+        return __left_contract[i];
+    }
+
+    Eigen::Tensor<Base, 4> right_contract(int i)
+    {
+        if(i==L)
+        {
+            Eigen::Tensor<Base, 4> res(1,1,1,1);
+            res(0,0,0,0) = 1;
+            res.leg_info = {Left1, Left2, Right1, Right2};
+            return res;
+        }
+        if(__right_contract[i].size()!=0)
+        {
+            return __right_contract[i];
+        }
+        if(i==L-1)
+        {
+            __right_contract[i] = Node::contract<1>(
+                lattice[i], lattice[i],
+                {Phy}, {Phy},
+                {{Left, Left1}, {Right, Right1}},
+                {{Left, Left2}, {Right, Right2}}
+            );
+        }
+        else
+        {
+            __right_contract[i] = Node::contract<2>(
+                right_contract(i+1),
+                Node::contract<1>(
+                    lattice[i],
+                    lattice[i],
+                    {Phy}, {Phy},
+                    {{Left, Left1}, {Right, Right1}},
+                    {{Left, Left2}, {Right, Right2}}
+                ),
+                {Left1, Left2}, {Right1, Right2});
+        }
+        return __right_contract[i];
+    }
+
+    MPS(int _D, int _L)
+    {
+        D = _D;
+        L = _L;
+        for(int i=0;i<L;i++)
+        {
+            lattice.push_back(Eigen::Tensor<Base, 3>{});
+            __left_contract.push_back(Eigen::Tensor<Base, 4>{});
+            __right_contract.push_back(Eigen::Tensor<Base, 4>{});
+        }
+        Hamiltonian = Eigen::TensorMap<Eigen::Tensor<Base, 4>> {
+            (Base *)Hamiltonian_data,
+            {2, 2, 2, 2}
+        } / Base(4);
+        Identity = Eigen::TensorMap<Eigen::Tensor<Base, 4>> {
+            (Base *)Identity_data,
+            {2, 2, 2, 2}
+        };
+        Update = Identity - delta_t * Hamiltonian;
+
         std::cout << "Created a MPS\n";
-        Hamiltonian = Hamiltonian/Base(4);
 
         for(int i=1;i<L-1;i++)
         {
@@ -32,13 +135,7 @@ struct MPS
         {
             lattice[i] = lattice[i].random()*Base(2) - Base(1);
             lattice[i].leg_info = {Left, Right, Phy};
-            //debug_tensor(lattice[i]);
         }
-        /*for(int i=0;i<L-1;i++)
-        {
-            env[i] = Eigen::Tensor<Base, 1>(D);
-            env[i].setConstant(Base(1));
-        }*/
     }
 
     void pre()
@@ -57,7 +154,7 @@ struct MPS
             auto big = Node::contract<1>(lattice[i], lattice[i+1],
                                       {Right}, {Left},
                                       {{Phy, Phy1}}, {{Phy, Phy2}});
-            auto Big = Node::contract<2>(big, Hamiltonian,
+            auto Big = Node::contract<2>(big, Update,
                                       {Phy1, Phy2}, {Phy1, Phy2});
             auto svd = Node::svd<2>(Big,
                                     {Left, Phy3}, Right, Left, D);
@@ -71,7 +168,7 @@ struct MPS
             auto big = Node::contract<1>(lattice[i], lattice[i-1],
                                       {Left}, {Right},
                                       {{Phy, Phy1}}, {{Phy, Phy2}});
-            auto Big = Node::contract<2>(big, Hamiltonian,
+            auto Big = Node::contract<2>(big, Update,
                                       {Phy1, Phy2}, {Phy1, Phy2});
             auto svd = Node::svd<2>(Big,
                                     {Right, Phy3}, Left, Right, D);
@@ -91,25 +188,58 @@ struct MPS
         for(int i=0;i<n;i++)
         {
             update();
+            std::cout << i << "\r";
         }
     }
-    Base norm()
+    Base energy_at_i_and_i_plus_1(int i)
     {
-        Eigen::Tensor<Base, 4> n = Node::contract(
-            lattice[0], lattice[0],
-            {Phy}, {Phy},
+        auto psi = Node::contract<1>(
+            lattice[i],
+            lattice[i+1],
+            {Right}, {Left},
+            {{Phy, Phy1}}, {{Phy, Phy2}}
+        );
+        auto Hpsi = Node::contract<2>(
+            psi,
+            Hamiltonian,
+            {Phy1, Phy2}, {Phy1, Phy2}
+        );
+        auto psiHpsi = Node::contract<2>(
+            Hpsi,
+            psi,
+            {Phy3, Phy4}, {Phy1, Phy2},
             {{Left, Left1}, {Right, Right1}},
             {{Left, Left2}, {Right, Right2}}
         );
-        //for(int i=0)
+        auto leftpsiHpsi = Node::contract<2>(
+            psiHpsi,
+            left_contract(i-1),
+            {Left1, Left2}, {Right1, Right2}
+        );
+        auto res = Node::contract<2>(
+            leftpsiHpsi,
+            right_contract(i+2),
+            {Right1, Right2}, {Left1, Left2}
+        );
+        return res(0,0,0,0);
+    }
+    Base energy()
+    {
+        Base total = Base(0);
+        for(int i=0;i<L-1;i++)
+        {
+            total += energy_at_i_and_i_plus_1(i);
+        }
+        total /= left_contract(L-1)(0,0,0,0);
+        return total/L;
     }
 };
 
-int main()
+int main(int n, char** argv)// D, L, T
 {
-    MPS<4, 10> mps{};
-    mps.update(100);
-    debug_tensor(mps.lattice[5]);
+    MPS<> mps {atoi(argv[1]), atoi(argv[2])};
+    mps.update(atoi(argv[3]));
+    std::cout << " E= " << mps.energy() << "\n";
     return 0;
 }
   
